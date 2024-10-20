711da71723dff9b2888c420bdbe7e9338d44eb6c
Sajal <sajalkumarr030@gmail.com>
2024-10-21 02:57:18 +0530

    hosted cors origin added

--- app/config/config.js
+++ app/config/config.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-  origin: "http://localhost:3000",
+  origin: ["http://localhost:3000", "https://lms-studium.up.railway.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
